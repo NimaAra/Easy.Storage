b87b17c3e73c272394816b8d2a615c69134c4cfc
Nima Ara <nima.ara@inbox.com>
2016-10-02 21:27:38 +0100

    Some awesomeness
    * Fixed a failing test;
    * Added nuspec.

--- Easy.Storage.Tests.Unit/SqlServer/Context.cs
+++ Easy.Storage.Tests.Unit/SqlServer/Context.cs
@@ -1,20 +1,12 @@
 ﻿namespace Easy.Storage.Tests.Unit.SqlServer
 {
-    using System;
     using System.IO;
-    using System.Reflection;
 
     internal class Context
     {
-        protected static string ConnectionString
-        {
-            get
-            {
-                var assembly = Assembly.GetExecutingAssembly();
-                var uri = new Uri(assembly.CodeBase);
-                return File.ReadAllText(Path.Combine(Path.GetDirectoryName(uri.LocalPath), "SqlServer\\connection.string"));
-            }
-        }
+        private static readonly FileInfo ConnectionStringsFile = new FileInfo("C:\\connection.string");
+
+        protected static string ConnectionString => File.ReadAllText(ConnectionStringsFile.FullName);
 
         protected static string TableQuery => "IF OBJECT_ID('Person', 'U') IS NULL" 
                                               + " CREATE TABLE Person (" 
@@ -25,15 +17,6 @@
         protected static string ViewQuery => "IF OBJECT_ID('Person_view', 'V') IS NULL\r\n"
                                               + "EXEC('CREATE VIEW Person_view AS SELECT * FROM Person')";
 
-        protected static bool IsRunningLocaly
-        {
-            get
-            {
-                var assembly = Assembly.GetExecutingAssembly();
-                var uri = new Uri(assembly.CodeBase);
-                var file = new FileInfo(Path.Combine(Path.GetDirectoryName(uri.LocalPath), "SqlServer\\connection.string"));
-                return file.Exists;
-            }
-        }
+        protected static bool IsRunningLocaly => ConnectionStringsFile.Exists;
     }
 }
