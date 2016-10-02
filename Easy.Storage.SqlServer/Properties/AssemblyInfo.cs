c007f58e8bd510346abf45e909ef0646cd18f038
Nima Ara <nima.ara@inbox.com>
2016-10-02 21:41:46 +0100

    Preparing for release

--- Easy.Storage.SqlServer/Properties/AssemblyInfo.cs
+++ Easy.Storage.SqlServer/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 
 [assembly: AssemblyTitle("EasyStorage.SqlServer")]
 
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 
 [assembly: AssemblyCopyright("Copyright ©  2016")]
